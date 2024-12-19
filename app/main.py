3316239dc75b0398601933b5e4f1cd4f460dc68a
nagafochfv <ohinskiy96@gmail.com>
2024-12-19 21:33:48 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -43,5 +43,7 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        dev_start = self.create_powerful_api() + self.create_awesome_web_page()
-        return dev_start
+        return (
+            BackendDeveloper.create_powerful_api(self)
+            + FrontendDeveloper.create_awesome_web_page(self)
+        )
